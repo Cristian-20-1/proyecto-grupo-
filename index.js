4b83e4b38b4081b2a750a9086798b8bb90b29c45
gabriel <120583307+gabriel-UwU@users.noreply.github.com>
2023-01-10 00:20:55 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+alert ("hello word")
+
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('estas en lo correcto es maricon ')
+});
+    
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',function () {
+    alert('incorrecto. es maricon ')
+});
